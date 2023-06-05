82667109e18d1342f9c144ad2398e3e2fced1af0
Ludivine <sagnol.ludivine@gmail.com>
2023-06-05 16:07:09 +0200

    feat(init): init project with dashboard page and book box page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DemoPageComponent } from './pages/demo/demo.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BookBoxComponent } from './pages/book-box/book-box.component';
 
 const routes: Routes = [
-  { path: '', component: DemoPageComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'book-box', component: BookBoxComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DemoPageComponent } from './pages/demo/demo.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BookBoxComponent } from './pages/book-box/book-box.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoPageComponent,
     DemoComponent,
     DemoPipe,
-    DemoDirective
+    DemoDirective,
+    DashboardComponent,
+    BookBoxComponent
   ],
   imports: [
     BrowserModule,
