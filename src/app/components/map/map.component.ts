f59fec64dd889f5ef4f09094a5b21100f857e80d
Mathilde Jnl <mathilde.jeannolle@gmail.com>
2023-06-08 21:50:56 +0200

    initialisation des popups permettant l'affichage des informations des boîtes à livres

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,20 +1,18 @@
-import { Component, Input } from '@angular/core';
-import * as Leaflet from 'leaflet';
-import { GeoShape, RecordBookBox } from 'src/app/utils/interface';
-import { BookBoxService } from 'src/app/services/bookBox/book-box.service';
+import { Component, Input } from "@angular/core";
+import { GeoShape, RecordBookBox } from "src/app/utils/interface";
+import { BookBoxService } from "src/app/services/bookBox/book-box.service";
+import * as L from "leaflet";
+import * as Leaflet from "leaflet";
 
-Leaflet.Icon.Default.mergeOptions({
-});
+Leaflet.Icon.Default.mergeOptions({});
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"],
 })
-
 export class MapComponent {
-
-  constructor(private bookBoxService: BookBoxService){}
+  constructor(private bookBoxService: BookBoxService) {}
 
   @Input() bookBox: RecordBookBox[] = [];
 
@@ -22,49 +20,68 @@ export class MapComponent {
 
   options = {
     layers: [
-      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      })
+      Leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }),
     ],
     zoom: 16,
-    center: { lat: 47.383331, lng: 0.68333 }
-  }
+    center: { lat: 47.383331, lng: 0.68333 },
+  };
 
   //quand carte prête, on initialise la map et les marqueurs
   onMapReady($event: Leaflet.Map) {
     this.map = $event;
     this.initMarkers();
   }
-    
+
   initMarkers() {
     //génère tableau vide qui contiendra les marqueurs
-    const initialMarkers: GeoShape[] = [];
+    const initialMarkers: any[] = [];
 
     //observable = event que l'on va observer ($ = convention pour mentionner que c'est un observable)
     //Fait l'appel d'API
-    const observableApiBookBox$  = this.bookBoxService.getBookBox();
+    const observableApiBookBox$ = this.bookBoxService.getBookBox();
     //.subscribe = va chercher la valeur de l'event = renvoie la rép de l'API
-    observableApiBookBox$ .subscribe({
+    observableApiBookBox$.subscribe({
       //une fois qu'API a répondu :
       next(apiBookBox) {
         //pour chaque valeur envoyée on l'envoie dans le tableau
-         apiBookBox.records.forEach(function(record) {
-          initialMarkers.push(record.fields.geo_shape);
+        apiBookBox.records.forEach(function (record) {
+          initialMarkers.push(record.fields);
         });
       },
       //Une fois tout ok, on les affiche
       complete() {
-        showMarker(initialMarkers)
+        showMarker(initialMarkers);
       },
     });
 
-    //on reprend le tableau, et pour chaque coordonée du tableau, on l'ajoute à la carte
-    const showMarker = (initialMarkers: GeoShape[]) => {
-      for (let i= 0; i < initialMarkers.length; i++) {
-        const marker = Leaflet.marker([initialMarkers[i].coordinates[1], initialMarkers[i].coordinates[0]]);
+    //on reprend le tableau, et pour chaque coordonnée du tableau, on l'ajoute à la carte
+    const showMarker = (initialMarkers: any) => {
+      for (let i = 0; i < initialMarkers.length; i++) {
+        const marker = Leaflet.marker([
+          initialMarkers[i].geo_shape.coordinates[1],
+          initialMarkers[i].geo_shape.coordinates[0],
+        ]);
+
+        let contenuPopup = initialMarkers[i].nom + "<br>" + initialMarkers[i].adresse + " " + initialMarkers[i].code_postal + " " + initialMarkers[i].commune;
+        
+        if(initialMarkers[i].gestionnaire){
+          contenuPopup += "<br>" + "Gestionnaire : " + initialMarkers[i].gestionnaire;
+        } else {
+          contenuPopup += "";
+        }
+       
+        if(initialMarkers[i].photo){
+          contenuPopup += "<br><img class=\"leaflet-popup-content-img\" width= 60%; src=\"" + initialMarkers[i].photo + "\">";
+        } else {
+          contenuPopup += "";
+        }
+        marker.bindPopup(contenuPopup).openPopup();
+
         marker.addTo(this.map);
       }
-    }
+    };
   }
-
 }
