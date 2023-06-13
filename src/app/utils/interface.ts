b8c3558b112d95b01b15c4640fba328d5ffe77a7
Ludivine <sagnol.ludivine@gmail.com>
2023-06-13 19:02:00 +0200

    feat(map): add input to add address manually

--- src/app/utils/interface.ts
+++ src/app/utils/interface.ts
@@ -84,3 +84,41 @@ export interface IUser {
     reading_in_progress: IBook[];
     reading_finished: IBook[];
 }
+
+export interface FeatureCollection {
+  type: string;
+  version: string;
+  features: Feature[];
+  attribution: string;
+  licence: string;
+  query: string;
+  limit: number;
+}
+
+export interface Feature {
+  type: string;
+  geometry: Geometry;
+  properties: Properties;
+}
+
+export interface Geometry {
+  type: string;
+  coordinates: [number, number];
+}
+
+export interface Properties {
+  label: string;
+  score: number;
+  housenumber: string;
+  id: string;
+  type: string;
+  name: string;
+  postcode: string;
+  citycode: string;
+  x: number;
+  y: number;
+  city: string;
+  context: string;
+  importance: number;
+  street: string;
+}
